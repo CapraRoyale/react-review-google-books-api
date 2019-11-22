feda67e213b17c7fe48e87d2e9ae448a66c2447c
CapraRoyale <siege.lehman@gmail.com>
2019-11-21 18:41:39 -0800

    Clear .ini files, more commenting on files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+// Import a specifci module from raect-router-dom and assign the names Router, Route and Switch
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Import components we've made for 
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
